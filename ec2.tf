00dfe3a3129b9c7f218c8f7072a0a17d9424f8ed
Swarna606 <56198970+Swarna606@users.noreply.github.com>
2021-07-15 12:20:35 +0530

    Update ec2.tf
    removing credentials

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,6 @@ resource "aws_instance" "ec2"{
 
 provider "aws" {
   region     = "us-east-1"
-  secret_key = "w6r5YESbE94PF+eozqZiExf8dED8rA/4nlGtQBBH"
-  access_key = "AKIA5S5EPWECVIHX5G2E"
+  
 }
 
